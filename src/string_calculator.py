4cc6a6cd79c30aca6f731e37ed9df8c11add3ae2
Chirag Bhalara <chiragbhalara95@gmail.com>
2025-09-15 22:18:50 +0530

    raise exception for negative numbers in add method

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -13,7 +13,14 @@ class StringCalculator:
             delimiter = re.escape(delimiter_line[2:])  # escape special chars like *
 
         parts = re.split(delimiter, numbers)
-        return sum(int(part) for part in parts)
+        nums = [int(part) for part in parts if part]
+
+        # Find negative numbers
+        negatives = [n for n in nums if n < 0]
+        if negatives:
+            raise ValueError(f"negative numbers not allowed {','.join(map(str, negatives))}")
+
+        return sum(nums)
 
 calc = StringCalculator()
 print(calc.add(""))      # 0
@@ -25,3 +32,7 @@ print(calc.add("1\n2,3"))    # 6
 print(calc.add("//;\n1;2"))     # 3
 print(calc.add("//|\n10|20|30")) # 60
 print(calc.add("//.\n5.5.5"))   # 15
+try:
+    print(calc.add("1,-2,3,-4"))
+except ValueError as e:
+    print(e)  # negative numbers not allowed -2,-4
