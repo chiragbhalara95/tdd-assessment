fe4d388e958730328ef77ed9861f3cc6aee47b1c
Chirag Bhalara <chiragbhalara95@gmail.com>
2025-09-15 22:11:48 +0530

    Support different delimiters

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -5,7 +5,14 @@ class StringCalculator:
         if numbers == "":
             return 0
 
-        parts = re.split(r"[,\n]", numbers)
+        delimiter = r"[,\n]"  # default delimiters
+
+        # Check for custom delimiter syntax: //{delimiter}\n
+        if numbers.startswith("//"):
+            delimiter_line, numbers = numbers.split("\n", 1)
+            delimiter = re.escape(delimiter_line[2:])  # escape special chars like *
+
+        parts = re.split(delimiter, numbers)
         return sum(int(part) for part in parts)
 
 calc = StringCalculator()
@@ -15,3 +22,6 @@ print(calc.add("1,5"))   # 6
 print(calc.add("1,5,6"))   #12
 print(calc.add("10,20,30,40,50"))   # 150
 print(calc.add("1\n2,3"))    # 6
+print(calc.add("//;\n1;2"))     # 3
+print(calc.add("//|\n10|20|30")) # 60
+print(calc.add("//.\n5.5.5"))   # 15
