7d644a986aa6114997c4ec2fa791c9a2270e3299
Chirag Bhalara <chiragbhalara95@gmail.com>
2025-09-15 22:05:03 +0530

    test add methods can support multiple number of input

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -6,7 +6,9 @@ class StringCalculator:
         parts = numbers.split(",")
         return sum(int(part) for part in parts)
 
-# calc = StringCalculator()
+calc = StringCalculator()
 # print(calc.add(""))      # 0
 # print(calc.add("1"))     # 1
 # print(calc.add("1,5"))   # 6
+# print(calc.add("1,5,6"))   #12
+# print(calc.add("10,20,30,40,50"))   # 150
