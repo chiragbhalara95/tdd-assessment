8a204b834ec6168196774567704e89dca980ced2
Chirag Bhalara <chiragbhalara95@gmail.com>
2025-09-15 22:09:07 +0530

    handle croma and new line for calculate sum

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,14 +1,17 @@
+import re
+
 class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
 
-        parts = numbers.split(",")
+        parts = re.split(r"[,\n]", numbers)
         return sum(int(part) for part in parts)
 
 calc = StringCalculator()
-# print(calc.add(""))      # 0
-# print(calc.add("1"))     # 1
-# print(calc.add("1,5"))   # 6
-# print(calc.add("1,5,6"))   #12
-# print(calc.add("10,20,30,40,50"))   # 150
+print(calc.add(""))      # 0
+print(calc.add("1"))     # 1
+print(calc.add("1,5"))   # 6
+print(calc.add("1,5,6"))   #12
+print(calc.add("10,20,30,40,50"))   # 150
+print(calc.add("1\n2,3"))    # 6
